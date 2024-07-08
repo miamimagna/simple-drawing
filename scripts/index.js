771f0d6819ed0de39d6a86779b7c4365982febff
miamimagna <138129055+miamimagna@users.noreply.github.com>
2024-07-09 00:42:13 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,34 @@
-const img = document.querySelector('div');
-const button = document.querySelector('button');
-
-img.style.visibility = 'visible';
-function toggle() {
-    img.style.visibility = img.style.visibility === 'visible' ? 'hidden' : 'visible';
+const div = document.querySelector('#container');
+const size = 5;
+function addDiv(x, y) {
+    const sdiv = document.createElement('div');
+    sdiv.classList.add('small');
+    sdiv.style.width = size + 'px';
+    sdiv.style.height = size + 'px';
+    sdiv.style.borderRadius = size / 2 + 'px';
+    sdiv.style.zIndex = '0';
+    div.appendChild(sdiv);
+    sdiv.style.position = 'absolute';
+    x -= size / 2, y -= size / 2;
+    sdiv.style.top = y + 'px';
+    sdiv.style.left = x + 'px';
 }
 
-button.addEventListener('click', toggle);
+let mouseState = false;
+function inn(e) {
+    mouseState = true;
+    addDiv(e.x, e.y);
+}
+function print(e) {
+    if (mouseState)
+        addDiv(e.x, e.y);
+}
+
+function out(e) {
+    addDiv(e.x, e.y);
+    mouseState = false;
+}
+
+div.addEventListener('mouseover', print);
+div.addEventListener('mousedown', inn)
+div.addEventListener('click', out);
