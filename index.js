e51c589bf43adc5abeac4a11f55445d4649483ca
miamimagna <138129055+miamimagna@users.noreply.github.com>
2024-07-08 22:47:26 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Background Image with Black Tint</title>
+  <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+  <div class="imgDummy"></div>
+  <button>show</button>
+  <script>
+    const img = document.querySelector('div');
+    const button = document.querySelector('button');
+
+    img.style.visibility = 'visible';
+    function toggle() {
+      img.style.visibility = img.style.visibility === 'visible' ? 'hidden' : 'visible';
+    }
+
+    button.addEventListener('click', toggle);
+  </script>
+</body>
+
+</html>
